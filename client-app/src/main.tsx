9b0e982568dd627ecd75832e3aa7c982144efba2
Wiktoria <wbronowska648@gmail.com>
2024-08-13 21:07:05 +0200

    Forms

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 import "react-calendar/dist/Calendar.css";
 import "./app/layout/styles.css";
 import { store, StoreContext } from "./app/stores/store";
