2533f1bbbabff1da2073d1245a69df3c9b932c4d
Wiktoria <wbronowska648@gmail.com>
2024-08-18 18:23:57 +0200

    Identity

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,9 +1,11 @@
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Activity = Domain.Activity;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
